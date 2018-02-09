6e961a151d77d22806c5d56cb356b750f6234631
Patrick Ritskes <patrickr@softpak.local>
2018-02-09 13:47:52 +0100

    Create the edit recipe page

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,25 +1,17 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
 
-/**
- * Generated class for the EditRecipePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
+  mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navParams: NavParams) {}
+
+  ngOnInit() {
+    this.mode  = this.navParams.get('mode');
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditRecipePage');
-  }
-
 }
