7c2c6c9d0cdfd522b6c49f88df171e34bb8e41e5
Patrick Ritskes <patrickr@softpak.local>
2018-02-09 11:35:49 +0100

    Add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
+import { RecipesPage } from '../pages/recipes/recipes';
+import { RecipePage } from '../pages/recipe/recipe';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
