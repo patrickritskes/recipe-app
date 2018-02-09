cdcb818d7df40aae2ce2efc5123e3e6e31ce0051
Patrick Ritskes <patrickr@softpak.local>
2018-02-09 12:10:03 +0100

    Add shopping list form with submit (template driven)

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
-/**
- * Generated class for the ShoppingListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onAddItem(form: NgForm) {
+    console.log(form);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
-  }
-
 }
